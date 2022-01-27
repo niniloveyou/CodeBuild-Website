c6521ae4f4b3381adcff6c57d41ca7ffb8e80568
zhangyuzhu <zhangyuzhu@youzan.com>
2022-01-27 17:35:51 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-
+      <header className="App-header">
+        <Button type="primary">Button</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
